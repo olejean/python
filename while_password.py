7c07e30c0adb672bd58de4f22781f902f3af2b37
olejean <ffpupkin@yandex.ru>
2023-04-13 17:20:36 +0300

    test1

--- while_password.py
+++ while_password.py
@@ -1,9 +1,10 @@
 message = ''
-
+password = []
 while message != 'secret':
     message = input('Введите пароль: ')
+    password.append(message)
     if message == 'secret':
         print('Коректный пароль')
         break
     print('не правильный пассворд')
-    
+    print(password)
